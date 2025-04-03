d51152cef90aa894ee7b9f14a1bea69eabffe35a
James <jamesholton2511@gmail.com>
2025-04-03 22:51:42 +1100

    Removed Dominos experience and added Petsitting project

---
+++ portfolio/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+<home>
+    <div class="navbar">
+      <div class="nav-item">Resume</div>
+      <div class="nav-item">About</div>
+      <div class="nav-item">Skills</div>
+      <div class="nav-item">Education</div>
+      <div class="nav-item">Contact</div>
+      <div class="nav-item">Socials</div>
+    </div>
+  <div class="name">
+    <div class="letter">J</div>
+    <div class="letter">A</div>
+    <div class="letter">M</div>
+    <div class="letter">E</div>
+    <div class="letter">S</div>
+  </div>
+</home>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+
+/* .nav-item {
+    display: flex;
+    justify-content: center;
+} */
+</style>
